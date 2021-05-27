40f00150ab105e5e754d291824a5e6cc0c7f6b2b
Grant Beschta <gbeschta@asu.edu>
2021-05-26 17:54:59 -0700

    Testing out length function on query list

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -261,7 +261,7 @@ class WebServer {
             for (int i = 0; i < array.size(); i++) {
               repo = (JSONObject)array.get(i);
               own = (JSONObject)repo.get("owner");
-              id = String.valueOf((long)own.get("id"()));
+              id = String.valueOf((long)own.get("id"));
               owner = (String)own.get("login");
               repoNames[i] = (String)repo.get("name");
             }
