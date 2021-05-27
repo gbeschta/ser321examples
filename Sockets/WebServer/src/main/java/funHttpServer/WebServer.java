fd3a0925db8761420b194e13776974f22a27a0a6
Grant Beschta <gbeschta@asu.edu>
2021-05-26 19:26:46 -0700

    Testing out length function on query list

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -272,6 +272,7 @@ class WebServer {
             for (int i = 0; i < array.size(); i++) {
               builder.append(owner+", "+id+" -> "+repoNames[i]+"\nmyeh");
             }
+            builder.append("12345");
           } catch(ParseException pe) {
             // Generate response
             builder.append("HTTP/1.1 412 Precondition Failed\n");
