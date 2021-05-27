07cf8caaf49f82186dab7d26b3f72a96c50a2b05
Grant Beschta <gbeschta@asu.edu>
2021-05-26 17:58:57 -0700

    Testing out length function on query list

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -270,7 +270,7 @@ class WebServer {
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
             for (int i = 0; i < array.size(); i++) {
-              builder.append(owner+", "+id+" -> "+repoNames[i]+"\n");
+              builder.append(owner+", "+id+" -> "+repoNames[i]+" \n");
             }
           } catch(ParseException pe) {
             // Generate response
